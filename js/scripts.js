5e724c791eabce4fc3985d99df1740336395abd8
Chris Crow and Chan Lee <chris@>
2018-08-08 16:51:13 -0700

    add toggle functionality for only the first created contact.

--- js/scripts.js
+++ js/scripts.js
@@ -14,9 +14,18 @@ $(document).ready(function() {
       "Email: " + $("input#email").val() + "<br>" +
       "Adress: " + $("input#adress1").val() + "<br>" +
       "City/State/Zip: " + $("input#address2").val();
+      //currently when we create a second contact, it's adding it twice. Once to the top level .addressbook, and once to the new .clickable div inside of that one.
+    $(".addressbook").append("<div>" + "</div>");
+    $(".addressbook div").addClass("clickable");
+    $(".clickable").append("<p>" + contactName + "</p>");
+    $(".clickable p:first").addClass("front");
+    $(".clickable").append("<p>" + contactPage + "</p>");
+    $(".clickable p:last").addClass("back");
+  });
 
-    $(".addressbook").append("<div>" + contactName + "</div>");
-    $(".addressbook").append("<div>" + contactPage + "</div>");
+  $(".addressbook").click(function() {
+    $(".front", this).toggle();
+    $(".back", this).toggle();
   });
 
 });
