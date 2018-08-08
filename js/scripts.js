6e2c700dce9df9e315bd7d6ee0e9f3e88200f46e
Chris Crow and Chan Lee <chan@>
2018-08-08 16:09:37 -0700

    show form as well as appending new contacts to address book

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,18 @@
 
 //front-end
 $(document).ready(function() {
+  $("#newcontact").submit(function(event) {
+    event.preventDefault();
+    var contactName = $("input#name").val();
+    var contactPage =
+      "Name: " + $("input#name").val() + "<br>" +
+      "Phone Number: " + $("input#phone").val() + "<br>" +
+      "Email: " + $("input#email").val() + "<br>" +
+      "Adress: " + $("input#adress1").val() + "<br>" +
+      "City/State/Zip: " + $("input#address2").val();
 
+    $(".addressbook").append("<div>" + contactName + "</div>");
+    $(".addressbook").append("<div>" + contactPage + "</div>");
+  });
 
 });
